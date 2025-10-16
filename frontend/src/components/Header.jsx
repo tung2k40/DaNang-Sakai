8156235fe0bd25ff6976e6954fa4805c05d5f1a2
VinhGiang731 <dinhvinhgiang345@gmail.com>
2025-10-16 22:04:23 +0700

    m

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,12 @@
+import logo from '../assets/images/logo.jpg';
+
 export default function Header() {
     return (
         <header className="bg-white] shadow px-6 py-4 flex justify-between items-center">
-            {/* <img src="./images/logo.jpg" alt="" /> */}
-            <h1 className="text-xl font-bold text-blue-600">My App</h1>
+            <div className="flex items-center">
+                <img className="w-10 h-10 mr-2" src={logo} alt="Logo" />
+                <h1 className="text-xl font-bold text-blue-600">My App</h1>
+            </div>
             <nav>
                 <ul className="flex gap-6 text-gray-700">
                     <li><a href="/">Home</a></li>
