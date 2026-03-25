62fcb03182c81e667bac7e9529ec8ddbfd28126e
tung2k40 <thanhtung26042004@.com>
2026-03-25 11:21:01 +0700

    fix UptimeRobot

--- backend/src/app.js
+++ backend/src/app.js
@@ -21,6 +21,11 @@ const app = express();
 
 const PORT = ENV.PORT || 3000;
 
+// API Health Check (Dành cho UptimeRobot)
+app.get('/', (req, res) => {
+    res.status(200).json({ status: 'OK', message: 'Server is awake!' });
+});
+
 app.use(cors);
 app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
